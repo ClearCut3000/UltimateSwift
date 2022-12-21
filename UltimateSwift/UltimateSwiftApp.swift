d88f2d0de996a16b0d60f5351376416c1e572351
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-21 23:54:31 +0300

    Initial Commit

---
+++ UltimateSwift/UltimateSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimateSwiftApp.swift
+//  UltimateSwift
+//
+//  Created by Николай Никитин on 21.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct UltimateSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
