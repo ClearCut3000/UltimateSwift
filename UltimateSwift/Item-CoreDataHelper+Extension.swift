f6af62ec792b2fe4b58d6747fc03fa676f87c92b
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-31 23:24:09 +0300

    Helper file for Item CoreData entity added

---
+++ UltimateSwift/Item-CoreDataHelper+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  Item-CoreDataHelper+Extension.swift
+//  UltimateSwift
+//
+//  Created by Николай Никитин on 31.12.2022.
+//
+
+import Foundation
+
+extension Item {
+  var itemTitle: String {
+    title ?? ""
+  }
+
+  var itemDetail: String {
+    detail ?? ""
+  }
+
+  var itemCreationDate: Date {
+    creationDate ?? Date()
+  }
+
+  static var example: Item {
+    let controller = DataController(inMemory: true)
+    let viewContext = controller.container.viewContext
+    let item = Item(context: viewContext)
+    item.title = "Example Item"
+    item.detail = "This is example item detail"
+    item.priority = 3
+    item.creationDate = Date()
+    return item
+  }
+}
