8671ee005f344951e7f39c222f05001b71ae0493
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-01-06 23:59:32 +0300

    Added Awards struct with static constants

---
+++ UltimateSwift/Award.swift
@@ -0,0 +1,21 @@
+//
+//  Award.swift
+//  UltimateSwift
+//
+//  Created by Николай Никитин on 06.01.2023.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
