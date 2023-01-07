fcf5b45d18c867c80795f073fa3a9e381ef61d31
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-01-07 23:10:27 +0300

    AwardView modifiers for awards description added

--- UltimateSwift/AwardsView.swift
+++ UltimateSwift/AwardsView.swift
@@ -11,6 +11,9 @@ struct AwardsView: View {
 
   //MARK: - View Properties
   static let tag: String? = "Awards"
+  @EnvironmentObject var dataController: DataController
+  @State private var selectedAward = Award.example
+  @State private var showingAwardDetails = false
   var columns: [GridItem] {
     [GridItem(.adaptive(minimum: 100, maximum: 100))]
   }
@@ -22,19 +25,31 @@ struct AwardsView: View {
         LazyVGrid(columns: columns) {
           ForEach(Award.allAwards) { award in
             Button {
-                // no action yet
+                selectedAward = award
+              showingAwardDetails = true
             } label: {
                 Image(systemName: award.image)
                     .resizable()
                     .scaledToFit()
                     .padding()
                     .frame(width: 100, height: 100)
-                    .foregroundColor(Color.secondary.opacity(0.5))
+                    .foregroundColor(dataController.hasEarned(award: award) ? Color(award.color) : Color.secondary.opacity(0.5))
             }
           }
         }
       }
       .navigationTitle("Awards")
+    }
+    .alert(isPresented: $showingAwardDetails) {
+      if dataController.hasEarned(award: selectedAward) {
+        return Alert(title: Text("Unlocked: \(selectedAward.name)"),
+                     message: Text("\(selectedAward.description)"),
+                     dismissButton: .default(Text("OK")))
+      } else {
+        return Alert(title: Text("Locked"),
+                     message: Text("\(selectedAward.description)"),
+                     dismissButton: .default(Text("OK")))
+      }
     }
   }
 }
