c67d90df88d9dd4e29c366ac42fb4fb386af4157
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-01-17 23:13:13 +0300

    Color extension added

---
+++ UltimateSwift/Extensions/Color-Additions+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions+Extension.swift
+//  UltimateSwift
+//
+//  Created by Николай Никитин on 17.01.2023.
+//
+
+import SwiftUI
+
+extension Color {
+  static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+  static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
