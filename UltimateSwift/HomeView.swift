3187ec9103901e6d1bbbd2bc64fb240db4065b31
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-01-18 19:51:16 +0300

    Added projects grid

--- UltimateSwift/HomeView.swift
+++ UltimateSwift/HomeView.swift
@@ -18,6 +18,9 @@ struct HomeView: View {
                                                    ascending: true)],
                 predicate: NSPredicate(format: "closed = false")) var projects: FetchedResults<Project>
   let items: FetchRequest<Item>
+  var rows: [GridItem] {
+    [GridItem(.fixed(100))]
+  }
 
   //MARK: - View Init
   init() {
@@ -33,7 +36,28 @@ struct HomeView: View {
   var body: some View {
     NavigationView {
       ScrollView {
-
+        VStack(alignment: .leading) {
+          ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: rows) {
+              ForEach(projects) { project in
+                VStack(alignment: .leading) {
+                  Text("\(project.projectItems.count) items")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                  Text(project.projectTitle)
+                    .font(.title2)
+                  ProgressView(value: project.completionAmount)
+                    .accentColor(Color(project.projectColor))
+                }
+                .padding()
+                .background(Color.secondarySystemGroupedBackground)
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.2), radius: 5)
+              }
+            }
+            .padding([.horizontal, .top])
+          }
+        }
       }
       .background(Color.systemGroupedBackground.ignoresSafeArea())
       .navigationTitle("Home")
