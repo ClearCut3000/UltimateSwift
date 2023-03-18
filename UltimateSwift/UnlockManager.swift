c5ddc0bade3d093f3fcddadf118293501ae63e6c
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-03-19 00:44:35 +0300

    Finished UnlockManager

--- UltimateSwift/UnlockManager.swift
+++ UltimateSwift/UnlockManager.swift
@@ -13,8 +13,8 @@ class UnlockManager: NSObject, ObservableObject, SKPaymentTransactionObserver, S
   // MARK: - Properties
   enum RequestState {
     case loading
-    case loaded
-    case failed
+    case loaded(SKProduct)
+    case failed(Error?)
     case purchased
     case deferred
   }
@@ -25,35 +25,84 @@ class UnlockManager: NSObject, ObservableObject, SKPaymentTransactionObserver, S
   private let request: SKProductsRequest
   // to store the list of products that were sent back
   private var loadedProducts = [SKProduct]()
+  private enum StoreError: Error {
+    case invalidIdentifiers, missingProduct
+  }
+  var canMakePayments: Bool {
+    SKPaymentQueue.canMakePayments()
+  }
 
   // MARK: - Init
   init(dataController: DataController) {
-      // Store the data controller we were sent.
-      self.dataController = dataController
-      // Prepare to look for our unlock product.
-      let productIDs = Set(["com.example.UltimateSwift.unlock"])
-      request = SKProductsRequest(productIdentifiers: productIDs)
-      // This is required because we inherit from NSObject.
-      super.init()
-      // Start watching the payment queue.
-      SKPaymentQueue.default().add(self)
-      // Set ourselves up to be notified when the product request completes.
-      request.delegate = self
-      // Start the request
-      request.start()
+    // Store the data controller we were sent.
+    self.dataController = dataController
+    // Prepare to look for our unlock product.
+    let productIDs = Set(["com.example.UltimateSwift.unlock"])
+    request = SKProductsRequest(productIdentifiers: productIDs)
+    // This is required because we inherit from NSObject.
+    super.init()
+    // Start watching the payment queue.
+    SKPaymentQueue.default().add(self)
+    guard dataController.fullVersionUnlocked == false else { return }
+    // Set ourselves up to be notified when the product request completes.
+    request.delegate = self
+    // Start the request
+    request.start()
   }
 
   // MARK: - Deinit
   deinit {
-      SKPaymentQueue.default().remove(self)
+    SKPaymentQueue.default().remove(self)
   }
 
   // MARK: - Methods
   func paymentQueue(_ queue: SKPaymentQueue, updatedTransactions transactions: [SKPaymentTransaction]) {
-
+    DispatchQueue.main.async { [self] in
+      for transaction in transactions {
+        switch transaction.transactionState {
+        case .purchased, .restored:
+          self.dataController.fullVersionUnlocked = true
+          self.requestState = .purchased
+          queue.finishTransaction(transaction)
+        case .failed:
+          if let product = loadedProducts.first {
+            self.requestState = .loaded(product)
+          } else {
+            self.requestState = .failed(transaction.error)
+          }
+          queue.finishTransaction(transaction)
+        case.deferred:
+          self.requestState = .deferred
+        default:
+          break
+        }
+      }
+    }
   }
 
   func productsRequest(_ request: SKProductsRequest, didReceive response: SKProductsResponse) {
-    
+    DispatchQueue.main.async {
+      // Store the returned products for later, if we need them.
+      self.loadedProducts = response.products
+      guard let unlock = self.loadedProducts.first else {
+        self.requestState = .failed(StoreError.missingProduct)
+        return
+      }
+      if response.invalidProductIdentifiers.isEmpty == false {
+        print("ALERT: Received invalid product identifiers: \(response.invalidProductIdentifiers)")
+        self.requestState = .failed(StoreError.invalidIdentifiers)
+        return
+      }
+      self.requestState = .loaded(unlock)
+    }
+  }
+
+  func buy(product: SKProduct) {
+    let payment = SKPayment(product: product)
+    SKPaymentQueue.default().add(payment)
+  }
+
+  func restore() {
+    SKPaymentQueue.default().restoreCompletedTransactions()
   }
 }
