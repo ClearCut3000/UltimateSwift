7c60b14e4d34bb1831276c57a346d2177484cdfc
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-03-27 21:40:15 +0300

    SKProduct extension for locale price

---
+++ UltimateSwift/Extensions/SKProduct-LocalizedPrice+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  SKProduct-LocalizedPrice+Extension.swift
+//  UltimateSwift
+//
+//  Created by Николай Никитин on 27.03.2023.
+//
+
+import StoreKit
+
+extension SKProduct {
+    var localizedPrice: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = priceLocale
+        return formatter.string(from: price)!
+    }
+}
