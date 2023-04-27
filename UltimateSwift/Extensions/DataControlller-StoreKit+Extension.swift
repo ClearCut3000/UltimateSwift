26b6d2d22a9a758e9613ad37646b2d0ec252d15c
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2023-04-27 23:42:46 +0300

    StoreKit datacontrdlller extension update

--- UltimateSwift/Extensions/DataControlller-StoreKit+Extension.swift
+++ UltimateSwift/Extensions/DataControlller-StoreKit+Extension.swift
@@ -12,10 +12,12 @@ extension DataController {
   /// Called when the app is launched to find the first active scene
   func appLaunched() {
     guard count(for: Project.fetchRequest()) >= 5 else { return }
+#if os(iOS)
     let allScenes = UIApplication.shared.connectedScenes
     let scene = allScenes.first { $0.activationState == .foregroundActive }
     if let windowScene = scene as? UIWindowScene {
       SKStoreReviewController.requestReview(in: windowScene)
     }
+#endif
   }
 }
